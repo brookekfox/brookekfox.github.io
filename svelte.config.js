c09ec9971446f919c5e512ab3360185dbf06ce5a
Brooke Fox <brookekfox@gmail.com>
2023-08-12 14:48:01 -0700

    static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
-
+export default {
   kit: {
     adapter: adapter(),
-  },
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    }
+  }
 };
-
-export default config;
